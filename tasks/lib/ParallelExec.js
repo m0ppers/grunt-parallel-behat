158f3f31697106e32e65ed0f455837d0a7788ef0
Andreas Streichardt <andreas.streichardt@gmail.com>
2014-01-02 14:11:45 +0100

    Run every command with a placeholder to enable different configurations per thread

--- tasks/lib/ParallelExec.js
+++ tasks/lib/ParallelExec.js
@@ -10,8 +10,14 @@ var exec = require('child_process').exec,
  */
 function ParallelExec (maxTasks, execOptions) {
     var running = false,
+        i,
         queue = [],
-        runningTasks = 0;
+        runningTasks = 0,
+        workers = [];
+
+    for (i = 0;i < maxTasks;i++) {
+        workers[i] = false;
+    }
 
     /**
      * True if there are items on the queue and we have space to run a task
@@ -35,6 +41,35 @@ function ParallelExec (maxTasks, execOptions) {
         }
     }
 
+    function findWorker() {
+        var workerId,
+            i;
+
+        for (i = 0;i < workers.length && typeof workerId == 'undefined';i++) {
+            if (workers[i] == false) {
+                workerId = i;
+            }
+        }
+        return workerId;
+    }
+
+    function startWorker(cmd) {
+        var workerId = findWorker(),
+            finalCmd;
+
+        if (typeof workerId === 'undefined') {
+            throw new Error('Could not find a free worker');
+        }
+        runningTasks++;
+        finalCmd = cmd.replace(/#workerId#/, workerId);
+        workers[workerId] = true;
+        var freeWorkerAndDone = function(cmd, err, stdout, stderr) {
+            workers[workerId] = false;
+            taskDone(cmd, err, stdout, stderr);
+        }
+        exec(finalCmd, execOptions, _.partial(freeWorkerAndDone, cmd));
+    }
+
     /**
      * Shift the next task from the queue and start it up
      */
@@ -42,9 +77,8 @@ function ParallelExec (maxTasks, execOptions) {
         if (canStartTask()) {
             var cmd = queue.shift();
 
-            runningTasks++;
             this.emit('startedTask', cmd);
-            exec(cmd, execOptions, _.partial(taskDone, cmd));
+            startWorker(cmd);
         }
     }
 
